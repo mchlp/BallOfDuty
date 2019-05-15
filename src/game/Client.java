5d906fab1369f30396961c73e979e1aede25a00e
Henry Gu <1520879+0@users.noreply.github.com>
2019-05-15 14:46:41 -0400

    Initial commit

---
+++ src/game/Client.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/game/Server.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
